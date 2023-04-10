5833359384121d720218a9c1ea8e5a7bef26f56d
C K Boyt <ckboyt@gmail.com>
2023-04-09 20:04:13 -0400

    Add initial routes for testing

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,10 +1,50 @@
 const router = require('express').Router();
-// const { Project, User } = require('../models');
-// const withAuth = require('../utils/auth');
+const { User, Log } = require('../models');
+const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
 
   res.render('homepage');
+
+});
+
+// TODO: withAuth middleware prevents access to route
+// router.get('/dashboard', withAuth, async (req, res) => {
+router.get('/dashboard', async (req, res) => {
+
+  try {
+
+    // TODO: find logged in user based on session ID
+    // const userData = await User.findByPk(req.session.user_id, {
+    //   attributes: { exclude: ['password'] }
+    // });
+
+    // TODO: const user = userData.get({ plain: true });
+
+    res.render('dashboard', {
+      // TODO: ...user,
+      // TODO: logged_in: true
+    });
+
+  } catch (err) {
+
+    res.status(500).json(err);
+
+  }
+
+});
+
+router.get('/login', (req, res) => {
+
+  // if user is already logged in, redirect to dashboard
+  if (req.session.logged_in) {
+
+    res.redirect('/dashboard');
+    return;
+
+  }
+
+  res.render('login');
 
 });
 
