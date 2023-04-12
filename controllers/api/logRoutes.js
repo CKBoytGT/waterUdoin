5f4c93b8d42d06c7e3e8c01f8f455cf1cdfa31cb
C K Boyt <ckboyt@gmail.com>
2023-04-12 05:26:44 -0400

    Fix require paths for models

--- controllers/api/logRoutes.js
+++ controllers/api/logRoutes.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
-const { Log } = require('../models');
+const { Log } = require('../../models');
 
-// code to post a new log (and overwrite current day's log for that user)
+// TODO: post a new log (and overwrite current day's log for that user)
+
+// TODO: get last 30 days logs for a user (based on user id?), inputting 0 for days not logged
+
+// TODO: get user's water goal
 
 module.exports = router;
